5484bd713ad1083f550226273e319217df036e2e
UnknownChick <ferreira.alexandre.2000@outlook.com>
2023-08-23 01:06:37 +0200

    reset

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once get_theme_file_path('/inc/hmr.php');
+
+// Ajouter la prise en charge des images mises en avant
+add_theme_support( 'post-thumbnails' );
+
+// Ajouter automatiquement le titre du site dans l'en-tête du site
+add_theme_support( 'title-tag' );
+
+function register_assets() {
+    // Déclarer le fichier style.css à la racine du thème
+    wp_enqueue_style('capitaine',
+        get_stylesheet_uri(), array(), '1.0');
+}
+add_action( 'wp_enqueue_scripts', 'register_assets' );
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+		  input: {
+			main: './sass/style.ts',   // Votre entrée JS/TS
+			style: './sass/style.scss'  // Votre entrée SASS/SCSS/CSS
+		  },
+		  output: {
+			dir: 'dist',  // le répertoire de sortie
+			format: 'es',  // Format du module. "es" est pour ES module.
+			entryFileNames: `[name].js`, // Format personnalisé pour les noms de fichiers d'entrée
+        	chunkFileNames: `[name].js`, // Format personnalisé pour les noms de fichiers chunk
+        	assetFileNames: `[name].[ext]` // Format personnalisé pour les noms de fichiers d'assets (comme CSS)
+		  }
+		}
+	  },
+	server: {
+	  port: 1337,
+	  host: '0.0.0.0',
+	},
+  });
